fbfa0316ecee71bb37a972a89704c08a361be82d
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-07-21 15:23:27 +0900

    Update

--- examples/classification.rs
+++ examples/classification.rs
@@ -19,13 +19,11 @@ fn argmax_1d(
 
     for (i, &value) in array.iter().enumerate() {
         if value > max_value {
-            // println!("{}: {}", i, value);
             max_index = i;
             max_value = value;
         }
     }
 
-    // ついでに softmax している
     Some((max_index, max_value))
 }
 
@@ -44,16 +42,14 @@ fn main() -> OrtResult<()> {
         .with_intra_threads(1)?
         .with_model_downloaded(MobileNet)?;
 
-    // ndarray mini-batches of 3-channel RGB images of shape (N x 3 x H x W)
     let array = Array4::from_shape_fn((1, 3, 224, 224), |_| rand::random::<f32>());
-    println!("{:?}", array);
+    println!("{:?}", session);
     let outputs: Vec<DynOrtTensor<ndarray::Dim<ndarray::IxDynImpl>>> =
         session.run([InputTensor::from_array(array.into_dyn())])?;
 
     let output: OrtOwnedTensor<f32, _> = outputs[0].try_extract().unwrap();
     let output_view = output.view().to_owned();
 
-    // println!("{:?}", output_view);
     let output_view = output_view.softmax(ndarray::Axis(1));
     let (argmax, max_value) = argmax_1d(&output_view).unwrap();
     println!("{}, {}", argmax, max_value);
